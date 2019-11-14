c9e59e4022fa86842057a26e57cebe8824a5dd38
Kamiada <kamiada528@gmail.com>
2019-11-14 18:56:11 +0000

    mm

---
+++ Coursework/DataAccessLayer/DEstoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Coursework.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Coursework.DataAccessLayer
+{
+    public class DEstoreContext : DbContext
+    {
+        public DEstoreContext() : base("DEstoreContext")
+        { 
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+
+}
---
+++ Coursework/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coursework.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public string SaleType { get; set; }
+
+    }
+}
